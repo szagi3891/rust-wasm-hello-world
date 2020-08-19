b8f7705be8a9f48bbdc85732d36134296d562e4f
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2020-08-19 07:09:47 +0200

    appState

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::collections::VecDeque;
+
 mod utils;
 
 use wasm_bindgen::prelude::*;
@@ -8,10 +13,62 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+//https://riptutorial.com/rust/example/25994/thread-local-objects
+
+struct AppItem {
+    current: u32
+}
+
+impl AppItem {
+    fn new(current: u32) -> AppItem {
+        AppItem {
+            current
+        }
+    }
+}
+
+impl Drop for AppItem {
+    fn drop(&mut self) {
+        let message = format!("AppItem Drop = {}", self.current);
+        consoleLog(message);
+    }
+}
+
+struct AppState {
+    counter: u32,
+    list: VecDeque<AppItem>
+}
+
+impl AppState {
+    fn new() -> AppState {
+        AppState {
+            counter: 1,
+            list: VecDeque::new()
+        }
+    }
+
+    fn getNextId(&mut self) -> u32 {
+        self.counter += 2;
+        let item = AppItem::new(self.counter);
+        self.list.push_back(item);
+
+        if self.list.len() > 5 {
+            self.list.pop_front();
+        }
+
+        self.counter
+    }
+}
+
+thread_local! {
+    static appState: RefCell<AppState> = RefCell::new(AppState::new());
+}
+
 #[wasm_bindgen]
 extern {
     fn alertConsole(s: &str);
     fn callFromWasm(message: &str);
+    fn consoleLog(message: String);
 }
 
 #[wasm_bindgen]
@@ -22,4 +79,15 @@ pub fn greet(message: &str, mes: &str) -> String {
     callFromWasm("Kończę alertowanie");
 
     "aaaaa kkklops".into()
+}
+
+
+#[wasm_bindgen]
+pub fn getNextId() -> u32 {
+    let nextId = appState.with(|state| {
+        let mut state = state.borrow_mut();
+        state.getNextId()
+    });
+
+    nextId
 }
--- www/index.js
+++ www/index.js
@@ -1,6 +1,9 @@
 //@ts-check
 
 import * as wasm from "wasm-game-of-life";
+window.consoleLog = (message) => {
+    console.info(`consoleLog => ${message}`);
+};
 
 window.alertConsole = (message) => {
     console.info(`alert console - ${message}`);
@@ -15,3 +18,8 @@ console.info('aaa');
 const aa = wasm.greet("dupa bladaaaaaa333", "66");
 console.info(`AA = ${aa}`);
 
+
+setInterval(() => {
+    const nextId = wasm.getNextId();
+    console.info(`next id === ${nextId}`);
+}, 1000);
